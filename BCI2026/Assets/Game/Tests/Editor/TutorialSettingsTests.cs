656ee721f3b4bd479cc9c5b853a2e26508ffb0a4
Gala Garcia <gala.garcia@outlook.es>
2026-08-09 09:49:02 +0200

    Add test to verify tutorial mental state color

--- BCI2026/Assets/Game/Tests/Editor/TutorialSettingsTests.cs
+++ BCI2026/Assets/Game/Tests/Editor/TutorialSettingsTests.cs
@@ -1,6 +1,7 @@
 using System;
 using BciGame.Core;
 using BciGame.UI;
+using Game.Scripts.Gameplay;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -18,6 +19,17 @@ namespace BciGame.Tests.Editor
                 if (id == TutorialTextId.None) { continue; }
                 Assert.That(settings.GetText(id), Is.Not.Empty, $"Missing text for {id}.");
             }
+        }
+
+        [Test]
+        public void GetColor_ReturnsColorForEveryMentalStateLevel()
+        {
+            TutorialSettings settings = Resources.Load<TutorialSettings>("TutorialSettings");
+            Assert.That(settings, Is.Not.Null, "TutorialSettings must be available in Resources.");
+            Assert.That(settings.GetColor(MentalStateLevel.None), Is.EqualTo(Color.white));
+            Assert.That(settings.GetColor(MentalStateLevel.Low), Is.EqualTo(new Color(0.04f, 0.52f, 1f)));
+            Assert.That(settings.GetColor(MentalStateLevel.Medium), Is.EqualTo(new Color(0.86f, 0.24f, 0.24f)));
+            Assert.That(settings.GetColor(MentalStateLevel.High), Is.EqualTo(new Color(0.86f, 0.24f, 0.24f)));
         }
     }
 }
